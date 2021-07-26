17ca3c3aa5e73dcb9641346ff52dbdb9f7f78e46
hadar-g <hadar.goldstein@gmail.com>
2021-07-26 15:30:31 -0700

    added post page and route to post page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Home from "./pages/Home"
 import CreatePost from './pages/CreatePost';
+import Post from './pages/Post';
 
 function App() {
 
@@ -10,11 +11,14 @@ function App() {
 
     <div className= 'App'>
       <Router>
-        <Link to ="/createpost"> Create A Post</Link>
-        <Link to = "/"> Go To Home Page</Link>
+        <div className ='navbar'>
+          <Link to ="/createpost"> Create A Post</Link>
+          <Link to = "/"> Go To Home Page</Link>
+        </div>
         <Switch>
           <Route path = "/" exact component={Home}/>
           <Route path = "/createpost" exact component={CreatePost}/>
+          <Route path = "/post/:id" exact component = {Post} />
         </Switch>
       </Router>
       
---
+++ client/src/pages/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            POST
+        </div>
+    )
+}
+
+export default Post
