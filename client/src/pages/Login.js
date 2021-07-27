65e3c769b6be1125663b9dbfdcadcd87eda5f38d
hadar-g <hadar.goldstein@gmail.com>
2021-07-27 00:07:35 -0700

    created log in and registration pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+
+
 
 function Login() {
+    const[userName, setUserName] = useState('')
+    const[password, setPassword] = useState('')
+
+    const login = () => {
+        const data = {username: userName, password: password}
+        axios.post("http://localhost:3001/auth/login", data).then((response) => {
+            console.log(response.data)
+        })
+    }
     return (
         <div>
-            <h1>Log in</h1>
+            <input 
+                type = "text"
+                onChange={(event) => {
+                    setUserName(event.target.value)
+                }}/>
+            <input 
+                type = "password"
+                onChange = {(event) => {
+                    setPassword(event.target.value)
+                }} />
+
+            <button onClick = {login}> Login</button>
         </div>
     )
 }
--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+import './CreatePost.css'
+import axios from 'axios'
+//import {useHistory} from 'react-router-dom'
 
 function Registration() {
+    const initialValues = {
+        username: "",
+        password: ""
+    }
+
+    const validationSchema = Yup.object().shape({
+        username: Yup.string().required("must input a title"),
+        password: Yup.string().min(4).max(20).required()
+    })
+
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3001/auth", data).then(() => {
+            console.log(data)
+        })
+    }
+
     return (
         <div>
-            <h1>Registration </h1>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                <Form className = "formContainer"> 
+                    <label>Username:</label>
+                    <ErrorMessage name="username" component="span"/>
+                    <Field 
+                        autoComplete ="off"
+                        id="inputCreateUser" 
+                        name ="username" 
+                        placeholder="your username..."/> 
+                    <label>Password:</label>
+                    <ErrorMessage name="password" component="span"/>
+                    <Field 
+                        autoComplete ="off"
+                        id="inputCreatePassword" 
+                        name ="password" 
+                        placeholder="your password..."
+                        type = "password"/> 
+                <button type = "submit">Register New User</button> 
+                </Form>
+            </Formik>
         </div>
     )
 }
