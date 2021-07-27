b12529361cb0b12b9a961433247cc73c54bc35ba
hadar-g <hadar.goldstein@gmail.com>
2021-07-26 22:26:27 -0700

    added router and login pages and routes to get to them

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Home from "./pages/Home"
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+
 
 function App() {
 
@@ -12,13 +15,17 @@ function App() {
     <div className= 'App'>
       <Router>
         <div className ='navbar'>
+          <Link to = "/"> Home</Link>
           <Link to ="/createpost"> Create A Post</Link>
-          <Link to = "/"> Go To Home Page</Link>
+          <Link to = "/login">Login</Link>
+          <Link to = "/registration"> Registration</Link>
         </div>
         <Switch>
           <Route path = "/" exact component={Home}/>
           <Route path = "/createpost" exact component={CreatePost}/>
           <Route path = "/post/:id" exact component = {Post} />
+          <Route path = "/login" exact component={Login}/>
+          <Route path = "/registration" exact component = {Registration}/>
         </Switch>
       </Router>
       
