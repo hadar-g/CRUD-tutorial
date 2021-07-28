831b0bd2b065fb760e9980208cd1cc5606734a87
hadar-g <hadar.goldstein@gmail.com>
2021-07-28 15:30:04 -0700

    end of porject

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -34,7 +34,6 @@ useEffect(() => {
                         {value.username}
                        
                     </div>
-                    
                 </div>
                
                  ) 
